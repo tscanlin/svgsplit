6600cce890451e2aaa76518b01a1810e2dd56429
Tim Scanlin <tscanlin@gmail.com>
2015-02-03 19:49:57 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# [SVGsplit](http://svgsplit.com)
+
+SVGsplit is a tool for splitting an SVG file into more / smaller files. It takes
+the svg groups in a file (<g> tags) and makes each group into its own file.
+Tools like Adobe Illustrator and Inkscape save each layer as a separate group,
+so if you are making icons and have an Illustrator file with a different icon
+on each layer, you can save the file as a .svg file and use this tool to make
+each layer into its own .svg file.
+
+This tool is available at: [http://svgsplit.com](http://svgsplit.com)
+
+## Limitations
+
+This won't work well if each layer has different sizes since <g> tags don't have
+a way to specify a size, so every layer should be the same size. However, with
+most applications this shouldn't be an issue. Open an issue if you find any
+issues or have a specific use case other than the intended in mind.
+
+## License
+
+This tool is MIT licensed so feel free to do whatever you want with it.
---
+++ js/main.js
@@ -0,0 +1,117 @@
+/* Scripts */
+
+$(document).ready(function() {
+    
+    function readfiles(files) {
+        var formData = tests.formdata ? new FormData() : null;
+        for (var i = 0; i < files.length; i++) {
+            if (tests.formdata)
+                formData.append('file', files[i]);
+
+            parseFile(files[i]);
+        }
+    }
+    
+    function parseFile(file) {
+        if (tests.filereader === true && acceptedTypes[file.type] === true) {
+            var reader = new FileReader();
+
+            reader.onload = function(event) {
+
+                holder.innerHTML = event.target.result;                
+                
+                var svgNode = $(holder).find("svg"),
+                result = event.target.result,
+                //lines = event.target.result.split("\n"),
+                items = [],
+                svgTag = result.match('<svg[^>]*>'),
+                svgHead = svgTag[0],
+                //console.log(svgTag);
+                svgFoot = "</svg>";
+                
+                while (svgNode.children().length < 2) {
+                    svgNode = svgNode.children();
+                }
+
+                for (var i = 0, length = svgNode.children().length; i < length; i++) {
+                    //console.log(svgNode.children().eq(i));
+                    var node = svgNode.children().eq(i)[0],
+                    id = svgNode.children().eq(i).removeAttr("display").attr("id"),
+                    nodeText = serializer.serializeToString(node),
+                    nodeText = svgHead + nodeText + svgFoot;
+                    //console.log(nodeText);
+                    zip.file(id+".svg", nodeText);
+                    
+                    // Add file previews below -------------------ADD files as images with data URI, download zip button
+                    items[i] = "<div>"+nodeText+"</div>";
+                    $("#split-svgs").append(items[i]);
+                }
+                                
+                content = zip.generate();
+                location.href="data:application/zip;base64," + content; // Enable for auto-download
+            };
+            
+            reader.readAsText(file);
+            
+        } else {
+            holder.innerHTML += '<p>Uploaded ' + file.name + ' ' + (file.size ? (file.size / 1024 | 0) + 'K' : '');
+            //console.log(file);
+        }
+    }
+
+    var holder = document.getElementById('holder'),
+        //svgElm = document.getElementById('svg-file'),
+        serializer = new XMLSerializer(),
+        zip = new JSZip(),
+        fileArray = [],
+        tests = {
+        filereader : typeof FileReader != 'undefined',
+        dnd : 'draggable' in document.createElement('span'),
+        formdata : !!window.FormData,
+        progress : "upload" in new XMLHttpRequest
+    }, support = {
+        filereader : document.getElementById('filereader'),
+        formdata : document.getElementById('formdata'),
+        progress : document.getElementById('progress')
+    }, acceptedTypes = {
+        'image/svg+xml' : true,
+        'image/png' : true,
+        'image/jpeg' : true,
+        'image/gif' : true
+    }, progress = document.getElementById('uploadprogress'), fileupload = document.getElementById('upload');
+
+    "filereader formdata progress".split(' ').forEach(function(api) {
+        if (tests[api] === false) {
+            support[api].className = 'fail';
+        } else {
+            // FFS. I could have done el.hidden = true, but IE doesn't support
+            // hidden, so I tried to create a polyfill that would extend the
+            // Element.prototype, but then IE10 doesn't even give me access
+            // to the Element object. Brilliant.
+            support[api].className = 'hidden';
+        }
+    });
+
+    if (tests.dnd) {
+        holder.ondragover = function() {
+            this.className = 'hover';
+            return false;
+        };
+        holder.ondragend = function() {
+            this.className = '';
+            return false;
+        };
+        holder.ondrop = function(e) {
+            this.className = '';
+            e.preventDefault();
+            readfiles(e.dataTransfer.files);
+        };
+    } else {
+        fileupload.className = 'hidden';
+        fileupload.querySelector('input').onchange = function() {
+            readfiles(this.files);
+        };
+    }
+
+
+}); 
